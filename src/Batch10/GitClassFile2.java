11c660f3b2ba4ee1440d6ced2ec20a4f0798b5c6
whitemanskotch <whitemanskotch@gmail.com>
2021-10-22 18:46:36 -0500

    the second commit

---
+++ src/Batch10/GitClassFile2.java
@@ -0,0 +1,10 @@
+package Batch10;
+
+public class GitClassFile2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Changes");
+	}
+
+}
---
+++ src/Batch10/GitClassFile3.java
@@ -0,0 +1,10 @@
+package Batch10;
+
+public class GitClassFile3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
