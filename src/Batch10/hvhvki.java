a012c1e33f4e628101941d29488aa1afb33a2e0d
whitemanskotch <whitemanskotch@gmail.com>
2021-10-22 19:03:53 -0500

    some commit

--- src/Batch10/hvhvki.java
+++ src/Batch10/hvhvki.java
@@ -1,10 +1,10 @@
 package Batch10;
 
-public class GitClassFile2 {
+public class hvhvki {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Changes");
+
 	}
 
 }
